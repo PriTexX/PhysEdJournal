2250a3aada5d9280370bcd930040bdab36b4fbc0
pritexx <vladon_2004@mail.ru>
2023-01-31 19:08:26 +0300

    add: basic SportType and WorkType

---
+++ PhysEdJournal.Core/Entities/Types/SportType.cs
@@ -0,0 +1,21 @@
+﻿namespace PhysEdJournal.Core.Entities.Types;
+
+public enum SportType
+{
+    ArmWrestling,
+    Basketball,
+    Volleyball,
+    WeightLifting,
+    Darts,
+    MartialArts,
+    Capoeira,
+    TableTennis,
+    GeneralPhysicalPreparation,
+    ImprovingPhysicalEducation,
+    Powerlifting,
+    Crossfit,
+    StepAerobics,
+    Gym,
+    Football,
+    AestheticGymnastics
+}
---
+++ PhysEdJournal.Core/Entities/Types/WorkType.cs
@@ -0,0 +1,17 @@
+﻿namespace PhysEdJournal.Core.Entities.Types;
+
+public enum WorkType
+{
+    Visit,
+    PhysicalTest,
+    ExternalFitness,
+    ExternalWorldwideCompetition,
+    ExternalNationalCompetition,
+    IndoorsCompetition,
+    TheoryTest,
+    TheoryEssay,
+    GTO,
+    ScientificConference,
+    ScientificArticle,
+    OnlineWork
+}
