73e68c73cb18fd6bf37b5ac4f93ba474dd696102
pritexx <vladon_2004@mail.ru>
2023-01-31 20:03:49 +0300

    add: Student.response.dto

---
+++ PhysEdJournal.Core/Entities/DTO/Student.response.dto.cs
@@ -0,0 +1,10 @@
+﻿namespace PhysEdJournal.Core.Entities.DTO;
+
+public record struct StudentResponseDto
+{
+    public string FullName { get; init; }
+    public string Guid { get; init; }
+    public string Group { get; init; }
+    public int Course { get; init; }
+    public string Department { get; init; }
+}
