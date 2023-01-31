338e24cc515c0826fd7d7b3d3f3d7ef71c363821
pritexx <vladon_2004@mail.ru>
2023-01-31 19:09:15 +0300

    add: Points.dto class

---
+++ PhysEdJournal.Core/Entities/DTO/Points.dto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PhysEdJournal.Core.Entities.Types;
+
+namespace PhysEdJournal.Core.Entities.DTO;
+
+public record struct PointsDto
+{
+    [Required]
+    public int Value { get; set; }
+    
+    [Required]
+    public DateOnly WorkDate { get; set; }
+    
+    public SportType SportType { get; set; }
+    
+    public WorkType WorkType { get; set; }
+    
+    public string? Comment { get; set; }
+}
