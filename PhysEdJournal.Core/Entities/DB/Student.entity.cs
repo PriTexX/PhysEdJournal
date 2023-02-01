b089e78f9a2cd1e61611aac69d53afd738dec513
pritexx <vladon_2004@mail.ru>
2023-02-01 16:03:52 +0300

    add: Student.entity

---
+++ PhysEdJournal.Core/Entities/DB/Student.entity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhysEdJournal.Core.Entities.DB;
+
+public class StudentEntity
+{
+    [Required] 
+    public string FullName { get; set; }
+    
+    [Required]
+    public string Group { get; set; }
+    
+    [Required]
+    [Range(1, 6)]
+    public int Course { get; set; }
+    
+    public int? HealthGroup { get; set; }
+    
+    public string? Department { get; set; } 
+}
