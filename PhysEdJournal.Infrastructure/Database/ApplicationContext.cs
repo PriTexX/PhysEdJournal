15074353bda82eb44bbbd9cf5b2f1c6cab55cd40
pritexx <vladon_2004@mail.ru>
2023-02-03 16:12:13 +0300

    add: ApplicationContext

---
+++ PhysEdJournal.Infrastructure/Database/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhysEdJournal.Core.Entities.DB;
+
+namespace PhysEdJournal.Infrastructure.Database;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<GroupEntity> Groups { get; set; }
+    public DbSet<StudentPointsHistoryEntity> StudentsPointsHistory { get; set; }
+    public DbSet<StudentVisitsHistoryEntity> StudentsVisitsHistory { get; set; }
+    public DbSet<StudentEntity> Students { get; set; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+    }
+}
