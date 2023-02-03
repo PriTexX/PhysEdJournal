b8ffd026dd4f59446a9c86f64b679591792b5cb8
pritexx <vladon_2004@mail.ru>
2023-02-03 16:11:21 +0300

    update: added new properties on Student entity

--- PhysEdJournal.Core/Entities/DB/Student.entity.cs
+++ PhysEdJournal.Core/Entities/DB/Student.entity.cs
@@ -1,20 +1,31 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhysEdJournal.Core.Entities.DB;
 
 public class StudentEntity
 {
+    [Key]
+    [Required]
+    public string StudentGuid { get; set; }
+    
     [Required] 
     public string FullName { get; set; }
     
     [Required]
     public string Group { get; set; }
+
+    [DefaultValue(0)]
+    public int Visits { get; set; }
     
     [Required]
     [Range(1, 6)]
     public int Course { get; set; }
     
     public int? HealthGroup { get; set; }
+
+    public string? Department { get; set; }
     
-    public string? Department { get; set; } 
+    public ICollection<StudentPointsHistoryEntity> StudentPointsHistory { get; set; }
+    public ICollection<StudentVisitsHistoryEntity> StudentVisitsHistory { get; set; }
 }
