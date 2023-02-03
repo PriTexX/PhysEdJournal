bcaddbce09149224fec77660507a8b41dfd36e39
pritexx <vladon_2004@mail.ru>
2023-02-03 16:10:39 +0300

    add: StudentPointsHistory.entity

---
+++ PhysEdJournal.Core/Entities/DB/StudentPointsHistory.entity.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using PhysEdJournal.Core.Entities.Types;
+
+namespace PhysEdJournal.Core.Entities.DB;
+
+public class StudentPointsHistoryEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    [Column(TypeName = "date")]
+    [Required]
+    public DateOnly Date { get; set; }
+    
+    [Required]
+    public WorkType WorkType { get; set; }
+    
+    [Required]
+    public int Points { get; set; }
+    
+    [Required]
+    public string StudentGuid { get; set; }
+    
+    [ForeignKey("StudentGuid")]
+    public StudentEntity Student { get; set; }
+}
---
+++ PhysEdJournal.Core/Entities/Types/GymType.cs
@@ -0,0 +1,14 @@
+﻿namespace PhysEdJournal.Core.Entities.Types;
+
+public enum GymType // Скорее всего не понадобится
+{
+    First = 1,
+    Second = 2,
+    Third = 3,
+    Fourth = 4,
+    Sixth = 6,
+    Seventh = 7,
+    Ninth = 9,
+    Izmaylovo = 10,
+    Avtozavod = 11
+}
