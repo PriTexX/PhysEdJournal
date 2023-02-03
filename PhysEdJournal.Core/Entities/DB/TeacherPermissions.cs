da382ad70730143204e77c1bb380803821744aaa
pritexx <vladon_2004@mail.ru>
2023-02-03 22:34:51 +0300

    add: Added TeacherPermissions enum

---
+++ PhysEdJournal.Core/Entities/DB/TeacherPermissions.cs
@@ -0,0 +1,10 @@
+﻿namespace PhysEdJournal.Core.Entities.DB;
+
+[Flags]
+public enum TeacherPermissions
+{
+    DefaultAccess = 0,
+    AdminAccess = 1,
+    SecretaryAccess = 2,
+    OnlineCourseAccess = 4
+}
