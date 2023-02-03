888c39a44afd9e1b874588650abf5c9d1413f35f
pritexx <vladon_2004@mail.ru>
2023-02-03 16:12:29 +0300

    add: DateOnly support for database

---
+++ PhysEdJournal.Infrastructure/Database/DateOnlySupport/DateOnlyComparer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace PhysEdJournal.Infrastructure.DateOnlySupport;
+
+public class DateOnlyComparer : ValueComparer<DateOnly>
+{
+    public DateOnlyComparer() : base(
+        (d1, d2) => d1.DayNumber == d2.DayNumber,
+        d => d.GetHashCode())
+    {
+    }
+}
---
+++ PhysEdJournal.Infrastructure/Database/DateOnlySupport/DateOnlyConverter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace PhysEdJournal.Infrastructure.DateOnlySupport;
+
+public class DateOnlyConverter : ValueConverter<DateOnly, DateTime>
+{
+    public DateOnlyConverter() : base(
+        dateOnly => dateOnly.ToDateTime(TimeOnly.MinValue),
+        dateTime => DateOnly.FromDateTime(dateTime))
+    {
+    }
+}
