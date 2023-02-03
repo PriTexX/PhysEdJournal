3362157e9880f5da5b075fd54cd501a91a709935
pritexx <vladon_2004@mail.ru>
2023-02-03 18:59:04 +0300

    update: added navigation properties to Teacher.entity

---
+++ PhysEdJournal.Core/Entities/DB/Teacher.entity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhysEdJournal.Core.Entities.DB;
+
+public class TeacherEntity
+{
+    [Key]
+    public string TeacherGuid { get; set; }
+    
+    [Required]
+    public string FullName { get; set; }
+    
+    public ICollection<GroupEntity> Groups { get; set; }
+}
