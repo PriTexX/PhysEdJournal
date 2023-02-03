4ee46941ad8d41c4c00c9762c6783724be804d66
pritexx <vladon_2004@mail.ru>
2023-02-03 22:35:30 +0300

    update: added Comment property on StudentPointsHistory.entity

--- PhysEdJournal.Core/Entities/DB/StudentPointsHistory.entity.cs
+++ PhysEdJournal.Core/Entities/DB/StudentPointsHistory.entity.cs
@@ -9,17 +9,19 @@ public class StudentPointsHistoryEntity
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    
+
     [Column(TypeName = "date")]
     [Required]
     public DateOnly Date { get; set; }
-    
+
+    [Required]
+    public int Points { get; set; }
+
     [Required]
     public WorkType WorkType { get; set; }
     
-    [Required]
-    public int Points { get; set; }
-    
+    public string? Comment { get; set; }
+
     [Required]
     public string StudentGuid { get; set; }
     
