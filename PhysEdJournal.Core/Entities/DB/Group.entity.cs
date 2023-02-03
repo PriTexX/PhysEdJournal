0fc08bf578e7d2af35bef50e18d3f2894c6a0cf3
pritexx <vladon_2004@mail.ru>
2023-02-03 16:10:15 +0300

    add: Group.entity.cs

---
+++ PhysEdJournal.Core/Entities/DB/Group.entity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhysEdJournal.Core.Entities.DB;
+
+public class GroupEntity
+{
+    [Key]
+    public string GroupName { get; set; }
+    
+    [DefaultValue(2.0)]
+    public double VisitValue { get; set; }
+    
+    public string? Curator { get; set; }
+}
