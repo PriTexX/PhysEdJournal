d84e6364aee5aa929b2c04d88cb9b92d5b630473
Lev Myts Yaroslavovich <leomyts@gmail.com>
2023-02-04 23:56:13 +0300

    Добавил Студент сервис, реализовал crud методы

---
+++ PhysEdJournal.Infrastructure/Services/StudentService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Mime;
+using PhysEdJournal.Core.Entities.DB;
+using PhysEdJournal.Infrastructure.Database;
+
+namespace PhysEdJournal.Infrastructure.Services;
+
+public sealed class StudentService
+{
+    private readonly ApplicationContext _applicationContext;
+    
+    public StudentService(ApplicationContext applicationContext)
+    {
+        _applicationContext = applicationContext;
+    }
+    
+    private void Create(StudentEntity studentEntity)
+    {
+        _applicationContext.Students.Add(studentEntity);
+    }
+
+    private async Task<StudentEntity?> Read(StudentEntity studentEntity)
+    {
+        var student = await _applicationContext.Students.FindAsync(studentEntity);
+
+        return student;
+    }
+
+    private void Update(StudentEntity studentEntity)
+    {
+        _applicationContext.Students.Update(studentEntity);
+    }
+
+    private void Delete(StudentEntity studentEntity)
+    {
+        _applicationContext.Students.Remove(studentEntity);
+    }
+}
