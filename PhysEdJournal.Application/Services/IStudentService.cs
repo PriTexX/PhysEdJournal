dc6864544f108573d4f681702342eb47dbd5047e
pritexx <vladon_2004@mail.ru>
2023-02-06 14:26:24 +0300

    add: IStudentService contract

---
+++ PhysEdJournal.Application/Services/IStudentService.cs
@@ -0,0 +1,23 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+using PhysEdJournal.Core.Entities.DB;
+using PhysEdJournal.Core.Entities.Types;
+
+namespace PhysEdJournal.Application.Services;
+
+public interface IStudentService
+{
+    public Task<Result<StudentPointsHistoryEntity>> AddPointsAsync(int pointsAmount, DateOnly date, WorkType workType,
+        string studentGuid, string? comment = null);
+
+    public Task<Result<StudentVisitsHistoryEntity>>
+        IncreaseVisitsAsync(string studentGuid, DateOnly date, string teacherGuid);
+
+    internal Task<Result<Unit>> CreateStudentAsync(StudentEntity studentEntity);
+    
+    public Task<Result<StudentEntity?>> GetStudentAsync(string guid);
+
+    public Task<Result<Unit>> UpdateStudentAsync(string guid, StudentEntity updatedStudent);
+
+    public Task<Result<Unit>> DeleteStudentAsync(string guid);
+}
