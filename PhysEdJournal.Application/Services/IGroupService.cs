47ba3724e1316c8e5a3c4d6293d96d8e03e71d98
Lev Myts Yaroslavovich <leomyts@gmail.com>
2023-02-23 13:35:53 +0300

    feature/GroupService

---
+++ PhysEdJournal.Application/Services/IGroupService.cs
@@ -0,0 +1,20 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+using PhysEdJournal.Core.Entities.DB;
+
+namespace PhysEdJournal.Application.Services;
+
+public interface IGroupService
+{
+    public Task<Result<Unit>> AssignCurator(string groupName, string teacherGuid);
+    
+    public Task<Result<Unit>> AssignVisitValue(string groupName, double newVisitValue);
+    
+    public Task<Result<Unit>> CreateGroupAsync(GroupEntity groupEntity);
+    
+    public Task<Result<GroupEntity?>> GetGroupAsync(string groupName);
+    
+    public Task<Result<Unit>> UpdateGroupAsync(GroupEntity updatedGroup);
+    
+    public Task<Result<Unit>> DeleteGroupAsync(string groupName);
+}
---
+++ PhysEdJournal.Infrastructure/Services/GroupService.cs
@@ -0,0 +1,118 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+using Microsoft.EntityFrameworkCore;
+using PhysEdJournal.Application.Services;
+using PhysEdJournal.Core.Entities.DB;
+using PhysEdJournal.Infrastructure.Database;
+
+namespace PhysEdJournal.Infrastructure.Services;
+
+public sealed class GroupService : IGroupService
+{
+    private readonly ApplicationContext _applicationContext;
+
+    public GroupService(ApplicationContext applicationContext)
+    {
+        _applicationContext = applicationContext;
+    }
+
+    public async Task<Result<Unit>> AssignCurator(string groupName, string teacherGuid)
+    {
+        var teacher = await _applicationContext.Teachers.FindAsync(teacherGuid);
+        
+        if (teacher == null)
+        {
+            return new Result<Unit>(new Exception("No such teacher found"));
+        }
+        
+        var group = await _applicationContext.Groups.FindAsync(groupName);
+        
+        if (group == null)
+        {
+            return new Result<Unit>(new Exception("No such group found"));
+        }
+
+        group.Curator = teacher;
+        group.CuratorGuid = teacher.TeacherGuid;
+        
+        await UpdateGroupAsync(group);
+
+        return Unit.Default;
+    }
+
+    public async Task<Result<Unit>> AssignVisitValue(string groupName, double newVisitValue)
+    {
+        var group = await _applicationContext.Groups.FindAsync(groupName);
+
+        if (group == null)
+        {
+            return new Result<Unit>(new Exception("No such group found"));
+        }
+
+        group.VisitValue = newVisitValue;
+
+        await UpdateGroupAsync(group);
+
+        return Unit.Default;
+    }
+
+    public async Task<Result<Unit>> CreateGroupAsync(GroupEntity groupEntity)
+    {
+        try
+        {
+            await _applicationContext.Groups.AddAsync(groupEntity);
+            await _applicationContext.SaveChangesAsync();
+        
+            return Unit.Default;
+        }
+        catch (Exception err)
+        {
+            return new Result<Unit>(err);
+        }
+    }
+
+    public async Task<Result<GroupEntity?>> GetGroupAsync(string groupName)
+    {
+        try
+        {
+            var group = await _applicationContext.Groups.FindAsync(groupName);
+            return group;
+        }
+        catch (Exception err)
+        {
+            return new Result<GroupEntity?>(err);
+        }
+    }
+
+    public async Task<Result<Unit>> UpdateGroupAsync(GroupEntity updatedGroup)
+    {
+        try
+        {
+            await _applicationContext.Groups.Where(g => g.GroupName == updatedGroup.GroupName)
+                .ExecuteUpdateAsync(p => p
+                    .SetProperty(g => g.Students, updatedGroup.Students)
+                    .SetProperty(g => g.VisitValue, updatedGroup.VisitValue)
+                    .SetProperty(g => g.Curator, updatedGroup.Curator)
+                    .SetProperty(g => g.CuratorGuid, updatedGroup.CuratorGuid));
+
+            return Unit.Default;
+        }
+        catch (Exception err)
+        {
+            return new Result<Unit>(err);
+        }
+    }
+
+    public async Task<Result<Unit>> DeleteGroupAsync(string groupName)
+    {
+        try
+        {
+            await _applicationContext.Groups.Where(g => g.GroupName == groupName).ExecuteDeleteAsync();
+            return Unit.Default;
+        }
+        catch (Exception err)
+        {
+            return new Result<Unit>(err);
+        }
+    }
+}
