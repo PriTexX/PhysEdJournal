55e530268c6fd3ecdd65f1b906f56b340dc2d743
pritexx <vladon_2004@mail.ru>
2023-02-26 12:29:58 +0300

    update: added new fields to query

--- PhysEdJournal.Api/GraphQL/Query.cs
+++ PhysEdJournal.Api/GraphQL/Query.cs
@@ -1,15 +1,19 @@
-﻿using PhysEdJournal.Core.Entities.DB;
+﻿using Microsoft.EntityFrameworkCore;
+using PhysEdJournal.Core.Entities.DB;
 using PhysEdJournal.Infrastructure.Database;
 
 namespace PhysEdJournal.Api.GraphQL;
 
 public class Query
 {
-    public StudentEntity? GetStudent([Service] ApplicationContext context)
+    [UseFirstOrDefault]
+    [UseProjection]
+    [UseFiltering]
+    public IQueryable<StudentEntity?> GetStudent([Service] ApplicationContext context, string guid)
     {
-        return context.Students.FirstOrDefault();
+        return context.Students.Where(s => s.StudentGuid == guid);
     }
-
+    
     [UseProjection]
     [UseFiltering]
     public IQueryable<StudentEntity> GetStudents([Service] ApplicationContext context)
@@ -22,6 +26,20 @@ public class Query
     public IQueryable<ArchivedStudentEntity> GetArchivedStudents([Service] ApplicationContext context)
     {
         return context.ArchivedStudents;
+    }
+    
+    [UseProjection]
+    [UseFiltering]
+    public IQueryable<PointsStudentHistoryEntity?> GetStudentPointsHistory([Service] ApplicationContext context, string guid)
+    {
+        return context.StudentsPointsHistory.Where(s => s.StudentGuid == guid);
+    }
+    
+    [UseProjection]
+    [UseFiltering]
+    public IQueryable<PointsStudentHistoryEntity?> GetPointsHistory([Service] ApplicationContext context)
+    {
+        return context.StudentsPointsHistory;
     }
 
     [UseProjection]
