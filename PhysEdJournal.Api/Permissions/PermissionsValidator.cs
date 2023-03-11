bb4b6564c206719db0198925dc589a415a14bc72
pritexx <vladon_2004@mail.ru>
2023-03-11 13:36:06 +0300

    started to implement access filtering

---
+++ PhysEdJournal.Api/Permissions/PermissionsValidator.cs
@@ -0,0 +1,43 @@
+﻿using PhysEdJournal.Core.Entities.Types;
+using PhysEdJournal.Core.Exceptions.TeacherExceptions;
+using PhysEdJournal.Infrastructure.Database;
+
+namespace PhysEdJournal.Api.Permissions;
+
+public class PermissionValidator
+{
+    private readonly ApplicationContext _applicationContext;
+
+    public PermissionValidator(ApplicationContext applicationContext)
+    {
+        _applicationContext = applicationContext;
+    }
+
+    public async Task<LanguageExt.Common.Result<bool>> ValidateTeacherPermissions(string teacherGuid, TeacherPermissions requiredPermissions)
+    {
+        var teacher = await _applicationContext.Teachers.FindAsync(teacherGuid);
+
+        if (teacher is null)
+        {
+            return new LanguageExt.Common.Result<bool>(new TeacherNotFoundException(teacherGuid));
+        }
+        
+        var hasEnough = HasEnoughPermissions(teacher.Permissions, requiredPermissions);
+
+        if (!hasEnough)
+            return new LanguageExt.Common.Result<bool>(new NotEnoughPermissionsException(teacherGuid, teacher.Permissions, requiredPermissions));
+
+        return hasEnough;
+    }
+
+    private static bool HasEnoughPermissions(TeacherPermissions permissions, TeacherPermissions requiredPermissions)
+    {
+        if (permissions.HasFlag(TeacherPermissions.AdminAccess))
+            return true;
+
+        if (requiredPermissions == 0)
+            return true;
+
+        return (permissions & requiredPermissions) != 0;
+    }
+}
