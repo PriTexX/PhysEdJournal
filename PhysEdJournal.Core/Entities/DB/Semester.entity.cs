e6778ce41d69c7243c646c077a18ec71cc6776ee
pritexx <vladon_2004@mail.ru>
2023-03-13 11:37:07 +0300

    added field to Semester.entity that specifies current active semester

--- PhysEdJournal.Core/Entities/DB/Semester.entity.cs
+++ PhysEdJournal.Core/Entities/DB/Semester.entity.cs
@@ -8,6 +8,8 @@ public sealed class SemesterEntity
     [RegularExpression(@"\d{4}-\d{4}/\w{5}")]  // 2022-2023/весна
     [Key]
     public string Name { get; set; }
+    
+    public bool IsCurrent { get; set; }
 
     public ICollection<ArchivedStudentEntity> ArchivedStudents { get; set; }
     public ICollection<PointsStudentHistoryEntity> StudentPointsHistories { get; set; }
