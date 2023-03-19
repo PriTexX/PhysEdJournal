4d5aa8693ddf20016710e47b7d92027dbaeaa965
Lev Myts Yaroslavovich <leomyts@gmail.com>
2023-03-19 22:08:02 +0300

    Создал проверку для визитов

---
+++ PhysEdJournal.Core/Exceptions/VisitsExceptions/DayOfVisitBiggerThanNowException.cs
@@ -0,0 +1,6 @@
+﻿namespace PhysEdJournal.Core.Exceptions.VisitsExceptions;
+
+public class DayOfVisitBiggerThanNowException : Exception
+{
+    public DayOfVisitBiggerThanNowException(DateOnly date): base($"{date} is later than current date"){}
+}
