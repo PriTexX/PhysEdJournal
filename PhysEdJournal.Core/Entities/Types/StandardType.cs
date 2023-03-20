b3c178e2fc6266b045849ffa5a88c528a0970b31
Lev Myts Yaroslavovich <leomyts@gmail.com>
2023-03-20 18:03:34 +0300

    Создал сущность для записи истории сдачи нормативов

---
+++ PhysEdJournal.Core/Entities/Types/StandardType.cs
@@ -0,0 +1,13 @@
+﻿namespace PhysEdJournal.Core.Entities.Types;
+
+public enum StandardType
+{
+    Tilts,                                         //Наклоны
+    Jumps,                                            // Прыжки
+    PullUps,                              //Подтягивания
+    Squats,                                            // Приседания
+    JumpingRopeJumps,                                              // Прыжки через скакалку
+    TorsoLifts,                               // Поднимания туловища
+    FlexionAndExtensionOfArms,                            // Сгибания и разгибания рук
+    ShuttleRun                             // Челночный бег
+}
