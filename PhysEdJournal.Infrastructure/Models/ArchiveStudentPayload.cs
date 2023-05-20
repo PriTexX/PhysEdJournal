16d004b1b4e8591d61e7370cfbd6a49d6c090528
pritexx <vladon_2004@mail.ru>
2023-05-20 13:17:34 +0300

    extracted archive logic into StudentArchiver class

---
+++ PhysEdJournal.Infrastructure/Models/ArchiveStudentPayload.cs
@@ -0,0 +1,14 @@
+﻿namespace PhysEdJournal.Infrastructure.Models;
+
+public sealed class ArchiveStudentPayload
+{
+    public required string StudentGuid { get; init; }
+    public required int Visits { get; init; }
+    public required string FullName { get; init; }
+    public required string GroupNumber { get; init; }
+    public required string CurrentSemesterName { get; init; }
+    public required string ActiveSemesterName { get; init; }
+    public required double TotalPoints { get; init; }
+    public required bool HasDebtFromPreviousSemester { get; init; }
+    
+}
