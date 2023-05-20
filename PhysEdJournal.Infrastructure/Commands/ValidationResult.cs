3b5a0bc4021b83a63ec12b696d78dcead66446ee
pritexx <vladon_2004@mail.ru>
2023-05-20 23:54:15 +0300

    added ValidationResult class that is returned from internals validators

---
+++ PhysEdJournal.Infrastructure/Commands/ValidationResult.cs
@@ -0,0 +1,19 @@
+﻿namespace PhysEdJournal.Infrastructure.Commands;
+
+internal struct ValidationResult
+{
+    public required bool IsSuccess { get; init; }
+    public bool IsFailed => !IsSuccess;
+    public Exception? ValidationException { get; init; }
+
+    public static implicit operator ValidationResult(Exception exc)
+    {
+        return new ValidationResult
+        {
+            IsSuccess = false,
+            ValidationException = new AggregateException(exc)
+        };
+    }
+
+    public static ValidationResult Success { get; } = new ValidationResult { IsSuccess = true };
+}
