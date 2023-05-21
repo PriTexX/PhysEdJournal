c47f1745cb19845fc39296373bb1652a4ada07cd
pritexx <vladon_2004@mail.ru>
2023-05-21 22:13:14 +0300

    created EmptyPayload for commands without args

---
+++ PhysEdJournal.Infrastructure/Commands/EmptyPayload.cs
@@ -0,0 +1,9 @@
+﻿namespace PhysEdJournal.Infrastructure.Commands;
+
+public sealed class EmptyPayload
+{
+    private EmptyPayload(){}
+    private static readonly EmptyPayload Instance = new ();
+
+    public EmptyPayload Empty => Instance;
+}
