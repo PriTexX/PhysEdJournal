7a524aaa57e5d0a1fec843de92957d6e95c4790a
Lev Myts Yaroslavovich <leomyts@gmail.com>
2023-05-22 14:30:53 +0300

    made commands from TeacherService

---
+++ PhysEdJournal.Infrastructure/Commands/CreateCompetitionCommand.cs
@@ -0,0 +1,32 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+using PhysEdJournal.Core.Entities.DB;
+using PhysEdJournal.Infrastructure.Commands.ValidationAndCommandAbstractions;
+using PhysEdJournal.Infrastructure.Database;
+
+namespace PhysEdJournal.Infrastructure.Commands;
+
+public sealed class CreateCompetitionCommandPayload
+{
+    public required string CompetitionName { get; init; }
+}
+
+public sealed class CreateCompetitionCommand : ICommand<CreateCompetitionCommandPayload, Unit>
+{
+    private readonly ApplicationContext _applicationContext;
+
+    public CreateCompetitionCommand(ApplicationContext applicationContext)
+    {
+        _applicationContext = applicationContext;
+    }
+    
+    public async Task<Result<Unit>> ExecuteAsync(CreateCompetitionCommandPayload commandPayload)
+    {
+        var comp = new CompetitionEntity{CompetitionName = commandPayload.CompetitionName};
+
+        _applicationContext.Competitions.Add(comp);
+        await _applicationContext.SaveChangesAsync();
+
+        return Unit.Default;
+    }
+}
---
+++ PhysEdJournal.Infrastructure/Commands/DeleteCompetitionCommand.cs
@@ -0,0 +1,35 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+using PhysEdJournal.Core.Exceptions.TeacherExceptions;
+using PhysEdJournal.Infrastructure.Commands.ValidationAndCommandAbstractions;
+using PhysEdJournal.Infrastructure.Database;
+
+namespace PhysEdJournal.Infrastructure.Commands;
+
+public sealed class DeleteCompetitionCommandPayload
+{
+    public required string CompetitionName { get; init; }
+}
+
+public sealed class DeleteCompetitionCommand : ICommand<DeleteCompetitionCommandPayload, Unit>
+{
+    private readonly ApplicationContext _applicationContext;
+
+    public DeleteCompetitionCommand(ApplicationContext applicationContext)
+    {
+        _applicationContext = applicationContext;
+    }
+    
+    public async Task<Result<Unit>> ExecuteAsync(DeleteCompetitionCommandPayload commandPayload)
+    {
+        var comp = await _applicationContext.Competitions.FindAsync(commandPayload.CompetitionName);
+
+        if (comp is null)
+            return new Result<Unit>(new CompetitionNotFoundException(commandPayload.CompetitionName));
+
+        _applicationContext.Competitions.Remove(comp);
+        await _applicationContext.SaveChangesAsync();
+
+        return Unit.Default;
+    }
+}
