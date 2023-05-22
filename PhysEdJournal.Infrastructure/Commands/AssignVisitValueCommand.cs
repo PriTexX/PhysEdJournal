f7df9714931337dd52d857f8dac7efb9d6d02349
Lev Myts Yaroslavovich <leomyts@gmail.com>
2023-05-22 12:59:23 +0300

    made commands from GroupService

---
+++ PhysEdJournal.Infrastructure/Commands/AssignVisitValueCommand.cs
@@ -0,0 +1,62 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+using PhysEdJournal.Core.Exceptions.GroupExceptions;
+using PhysEdJournal.Infrastructure.Commands.ValidationAndCommandAbstractions;
+using PhysEdJournal.Infrastructure.Database;
+
+namespace PhysEdJournal.Infrastructure.Commands;
+
+public sealed class AssignVisitValueCommandPayload
+{
+    public required string GroupName { get; init; }
+    public required double NewVisitValue { get; init; }
+}
+
+internal sealed class AssignVisitValueCommandValidator : ICommandValidator<AssignVisitValueCommandPayload>
+{
+    public async ValueTask<ValidationResult> ValidateCommandInputAsync(AssignVisitValueCommandPayload commandInput)
+    {
+        if (commandInput.NewVisitValue <= 0)
+        {
+            return new NullVisitValueException(commandInput.NewVisitValue);
+        }
+
+        return ValidationResult.Success;
+    }
+} 
+
+public sealed class AssignVisitValueCommand : ICommand<AssignVisitValueCommandPayload, Unit>
+{
+    private readonly ApplicationContext _applicationContext;
+    private readonly AssignVisitValueCommandValidator _validator;
+
+    public AssignVisitValueCommand(ApplicationContext applicationContext)
+    {
+        _applicationContext = applicationContext;
+        _validator = new AssignVisitValueCommandValidator();
+    }
+    
+    public async Task<Result<Unit>> ExecuteAsync(AssignVisitValueCommandPayload commandPayload)
+    {
+        var validationResult = await _validator.ValidateCommandInputAsync(commandPayload);
+        
+        if (validationResult.IsFailed)
+        {
+            return new Result<Unit>(validationResult.ValidationException);
+        }
+        
+        var group = await _applicationContext.Groups.FindAsync(commandPayload.GroupName);
+
+        if (group == null)
+        {
+            return new Result<Unit>(new GroupNotFoundException(commandPayload.GroupName));
+        }
+
+        group.VisitValue = commandPayload.NewVisitValue;
+
+        _applicationContext.Groups.Update(group);
+        await _applicationContext.SaveChangesAsync();
+
+        return new Result<Unit>(Unit.Default);
+    }
+}
