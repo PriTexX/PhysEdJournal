0267c5ccef26a61b0bb018a641c29dea3fd3b4c8
Влад Булавин <vladon_2004@mail.ru>
2023-05-28 05:21:16 +0300

    added implicit ValueTask support for ValidationResult

--- PhysEdJournal.Infrastructure/Commands/ValidationAndCommandAbstractions/ValidationResult.cs
+++ PhysEdJournal.Infrastructure/Commands/ValidationAndCommandAbstractions/ValidationResult.cs
@@ -6,6 +6,15 @@ internal struct ValidationResult
     public bool IsFailed => !IsSuccess;
     public Exception? ValidationException { get; init; }
 
+    public static ValidationResult Create(Exception exc)
+    {
+        return new ValidationResult
+        {
+            IsSuccess = false,
+            ValidationException = new AggregateException(exc)
+        };
+    }
+    
     public static implicit operator ValidationResult(Exception exc)
     {
         return new ValidationResult
@@ -13,6 +22,11 @@ internal struct ValidationResult
             IsSuccess = false,
             ValidationException = new AggregateException(exc)
         };
+    }
+
+    public static implicit operator ValueTask<ValidationResult>(ValidationResult result)
+    {
+        return ValueTask.FromResult(result);
     }
 
     public static ValidationResult Success { get; } = new ValidationResult { IsSuccess = true };
