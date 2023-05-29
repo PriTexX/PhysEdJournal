749e0b793d4100100dd0f9fd1b3fb88f177d071c
Влад Булавин <vladon_2004@mail.ru>
2023-05-29 05:11:10 +0300

    implemented 4 more commands

---
+++ PhysEdJournal.Infrastructure/Commands/AdminCommands/ActivateStudentCommand.cs
@@ -0,0 +1,33 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+using Microsoft.EntityFrameworkCore;
+using PhysEdJournal.Core.Exceptions.StudentExceptions;
+using PhysEdJournal.Infrastructure.Commands.ValidationAndCommandAbstractions;
+using PhysEdJournal.Infrastructure.Database;
+
+namespace PhysEdJournal.Infrastructure.Commands.AdminCommands;
+
+public sealed class ActivateStudentCommand : ICommand<string, Unit>
+{
+    private readonly ApplicationContext _applicationContext;
+
+    public ActivateStudentCommand(ApplicationContext applicationContext)
+    {
+        _applicationContext = applicationContext;
+    }
+
+    public async Task<Result<Unit>> ExecuteAsync(string studentGuid)
+    {
+        var rowsAffected = await _applicationContext.Students
+            .Where(s => s.StudentGuid == studentGuid)
+            .ExecuteUpdateAsync(p => p
+                .SetProperty(s => s.IsActive, true));
+
+        if (rowsAffected == 0)
+        {
+            return new Result<Unit>(new StudentNotFoundException(studentGuid));
+        }
+
+        return Unit.Default;
+    }
+}
---
+++ PhysEdJournal.Infrastructure/Commands/AdminCommands/UnArchiveStudentCommand.cs
@@ -0,0 +1,80 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+using Microsoft.EntityFrameworkCore;
+using PhysEdJournal.Core.Exceptions.StudentExceptions;
+using PhysEdJournal.Infrastructure.Commands.ValidationAndCommandAbstractions;
+using PhysEdJournal.Infrastructure.Database;
+
+namespace PhysEdJournal.Infrastructure.Commands.AdminCommands;
+
+public sealed class UnArchiveStudentCommandPayload
+{
+    public required string StudentGuid { get; init; }
+    public required string SemesterName { get; init; }
+}
+
+public sealed class UnArchiveStudentCommand : ICommand<UnArchiveStudentCommandPayload, Unit>
+{
+    private readonly ApplicationContext _applicationContext;
+
+    public UnArchiveStudentCommand(ApplicationContext applicationContext)
+    {
+        _applicationContext = applicationContext;
+    }
+
+    public async Task<Result<Unit>> ExecuteAsync(UnArchiveStudentCommandPayload commandPayload)
+    {
+        var student = await _applicationContext.Students.FindAsync(commandPayload.StudentGuid);
+            if (student is null)
+            {
+                return new Result<Unit>(new StudentNotFoundException(commandPayload.StudentGuid));
+            }
+        
+            var archivedStudent = await _applicationContext.ArchivedStudents.FindAsync(commandPayload.StudentGuid, commandPayload.SemesterName);
+            if (archivedStudent is null)
+            {
+                return new Result<Unit>(new ArchivedStudentNotFound(commandPayload.StudentGuid, commandPayload.SemesterName));
+            }
+
+            await using var transaction = await _applicationContext.Database.BeginTransactionAsync();
+            
+            var pointsStudentHistory = await _applicationContext.PointsStudentsHistory
+                .Where(h => h.StudentGuid == commandPayload.StudentGuid && h.SemesterName == commandPayload.SemesterName)
+                .ToListAsync();
+    
+            student.AdditionalPoints = pointsStudentHistory.Aggregate(0, (prev, next) => prev + next.Points);
+            
+            var standardsStudentHistory = await _applicationContext.StandardsStudentsHistory
+                .Where(h => h.StudentGuid == commandPayload.StudentGuid && h.SemesterName == commandPayload.SemesterName)
+                .ToListAsync();
+            
+            student.PointsForStandards = standardsStudentHistory.Aggregate(0, (prev, next) => prev + next.Points);
+    
+            student.HasDebtFromPreviousSemester = false;
+            student.ArchivedVisitValue = 0;
+            student.Visits = archivedStudent.Visits;
+
+            _applicationContext.Students.Update(student);
+            await _applicationContext.SaveChangesAsync();
+
+            await _applicationContext.VisitsStudentsHistory
+                .Where(h => h.StudentGuid == commandPayload.StudentGuid && h.IsArchived == true)
+                .ExecuteUpdateAsync(p => p
+                    .SetProperty(s => s.IsArchived, false));
+
+            await _applicationContext.PointsStudentsHistory
+                .Where(h => h.StudentGuid == commandPayload.StudentGuid && h.SemesterName == commandPayload.SemesterName)
+                .ExecuteUpdateAsync(p => p
+                    .SetProperty(s => s.SemesterName, student.CurrentSemesterName)
+                    .SetProperty(s => s.IsArchived, false));
+            
+            await _applicationContext.StandardsStudentsHistory
+                .Where(h => h.StudentGuid == commandPayload.StudentGuid && h.SemesterName == commandPayload.SemesterName)
+                .ExecuteUpdateAsync(p => p
+                    .SetProperty(s => s.SemesterName, student.CurrentSemesterName)
+                    .SetProperty(s => s.IsArchived, false));
+
+            await transaction.CommitAsync();
+            return Unit.Default;
+    }
+}
