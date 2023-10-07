6214279d9be7215e7127ca5cf644e4d4627e9fcd
pritexx <vladon_2004@mail.ru>
2023-10-07 17:03:23 +0300

    added field restrictions migration

---
+++ PhysEdJournal.Infrastructure/Migrations/20231007125348_field-restrictions.cs
@@ -0,0 +1,436 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhysEdJournal.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fieldrestrictions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TeacherGuid",
+                table: "VisitsStudentsHistory",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentGuid",
+                table: "VisitsStudentsHistory",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Teachers",
+                type: "character varying(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TeacherGuid",
+                table: "Teachers",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GroupNumber",
+                table: "Students",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Students",
+                type: "character varying(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Department",
+                table: "Students",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CurrentSemesterName",
+                table: "Students",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentGuid",
+                table: "Students",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TeacherGuid",
+                table: "StandardsStudentsHistory",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentGuid",
+                table: "StandardsStudentsHistory",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SemesterName",
+                table: "StandardsStudentsHistory",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Semesters",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TeacherGuid",
+                table: "PointsStudentsHistory",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentGuid",
+                table: "PointsStudentsHistory",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SemesterName",
+                table: "PointsStudentsHistory",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CuratorGuid",
+                table: "Groups",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GroupName",
+                table: "Groups",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompetitionName",
+                table: "Competitions",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GroupNumber",
+                table: "ArchivedStudents",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "ArchivedStudents",
+                type: "character varying(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SemesterName",
+                table: "ArchivedStudents",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentGuid",
+                table: "ArchivedStudents",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TeacherGuid",
+                table: "VisitsStudentsHistory",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentGuid",
+                table: "VisitsStudentsHistory",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Teachers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TeacherGuid",
+                table: "Teachers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GroupNumber",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Department",
+                table: "Students",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CurrentSemesterName",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentGuid",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TeacherGuid",
+                table: "StandardsStudentsHistory",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentGuid",
+                table: "StandardsStudentsHistory",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SemesterName",
+                table: "StandardsStudentsHistory",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Semesters",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TeacherGuid",
+                table: "PointsStudentsHistory",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentGuid",
+                table: "PointsStudentsHistory",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SemesterName",
+                table: "PointsStudentsHistory",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CuratorGuid",
+                table: "Groups",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GroupName",
+                table: "Groups",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompetitionName",
+                table: "Competitions",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GroupNumber",
+                table: "ArchivedStudents",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "ArchivedStudents",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SemesterName",
+                table: "ArchivedStudents",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentGuid",
+                table: "ArchivedStudents",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36);
+        }
+    }
+}
