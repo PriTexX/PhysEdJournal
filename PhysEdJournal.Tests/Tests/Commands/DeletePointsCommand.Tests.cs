91390ed79c0d113ad2c8e1b758a8614cd54d7154
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-10-14 00:57:18 +0300

    Написал тесты

--- PhysEdJournal.Tests/Tests/Commands/DeletePointsCommand.Tests.cs
+++ PhysEdJournal.Tests/Tests/Commands/DeletePointsCommand.Tests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using PhysEdJournal.Core.Entities.DB;
 using PhysEdJournal.Core.Entities.Types;
+using PhysEdJournal.Core.Exceptions.PointsExceptions;
+using PhysEdJournal.Core.Exceptions.StudentExceptions;
 using PhysEdJournal.Infrastructure.Commands;
 using PhysEdJournal.Tests.Setup;
 using PhysEdJournal.Tests.Setup.Utils;
@@ -9,26 +10,32 @@ namespace PhysEdJournal.Tests.Tests.Commands;
 
 public sealed class DeletePointsCommandTests : DatabaseTestsHelper
 {
-    [Fact]
-    public async Task DeletePointsAsync_DeletePointsFromStudent_ShouldWorkProperly()
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(6)]
+    [InlineData(7)]
+    [InlineData(8)]
+    [InlineData(9)]
+    [InlineData(10)]
+    [InlineData(100)]
+    public async Task DeletePointsAsync_DeletePointsFromStudent_ShouldWorkProperly(int additionalPoints)
     {
         // Arrange
         await using var context = CreateContext();
         await ClearDatabase(context);
         
+        var expectedAdditionalPoints = additionalPoints - 10;
+        
         var command = new DeletePointsCommand(context);
         var semester = EntitiesFactory.CreateSemester("2022-2023/spring", true);
         var group = EntitiesFactory.CreateGroup("211-729");
-        var student = EntitiesFactory.CreateStudent(group.GroupName, semester.Name, false, true);
+        var student = EntitiesFactory.CreateStudent(group.GroupName, semester.Name, false, true, additionalPoints);
         var teacher = EntitiesFactory.CreateTeacher(permissions: TeacherPermissions.SuperUser);
         var historyEntity = EntitiesFactory.CreatePointsStudentHistoryEntity(student.StudentGuid, WorkType.Activist, teacher.TeacherGuid, DateOnlyGenerator.GetWorkingDate(), 10, semester.Name);
-        var payload = new DeletePointsCommandPayload
-        {
-            StudentGuid = historyEntity.StudentGuid,
-            TeacherGuid = historyEntity.TeacherGuid,
-            HistoryId = 1,
-            IgnoreDateIntervalCheck = false,
-        };
 
         await context.Semesters.AddAsync(semester);
         await context.Groups.AddAsync(group);
@@ -36,6 +43,15 @@ public sealed class DeletePointsCommandTests : DatabaseTestsHelper
         await context.Teachers.AddAsync(teacher);
         await context.PointsStudentsHistory.AddAsync(historyEntity);
         await context.SaveChangesAsync();
+        
+        var historyObj = context.PointsStudentsHistory.FirstOrDefault(h => h.Points == historyEntity.Points);
+        var payload = new DeletePointsCommandPayload
+        {
+            StudentGuid = historyEntity.StudentGuid,
+            TeacherGuid = historyEntity.TeacherGuid,
+            HistoryId = historyObj!.Id,
+            IgnoreDateIntervalCheck = false,
+        };
 
         // Act
         var result = await command.ExecuteAsync(payload);
@@ -46,7 +62,183 @@ public sealed class DeletePointsCommandTests : DatabaseTestsHelper
         var studentFromDb = await assertContext.Students.Include(s => s.PointsStudentHistory)
             .Where(s => s.StudentGuid == student.StudentGuid).FirstOrDefaultAsync();
         Assert.NotNull(studentFromDb);
-        var duplicate = studentFromDb.PointsStudentHistory.FirstOrDefault(h => h.Points == historyEntity.Points);
-        Assert.Null(duplicate);
+        Assert.Equal(expectedAdditionalPoints, studentFromDb.AdditionalPoints);
+        var historyEntityFromDb = studentFromDb.PointsStudentHistory.FirstOrDefault(h => h.Points == historyEntity.Points);
+        Assert.Null(historyEntityFromDb);
+    }
+    
+    [Fact]
+    public async Task DeletePointsAsync_DeletePointsFromStudentForce_ShouldWorkProperly()
+    {
+        // Arrange
+        await using var context = CreateContext();
+        await ClearDatabase(context);
+        
+        const int expectedAdditionalPoints = 0;
+        
+        var command = new DeletePointsCommand(context);
+        var semester = EntitiesFactory.CreateSemester("2022-2023/spring", true);
+        var group = EntitiesFactory.CreateGroup("211-729");
+        var student = EntitiesFactory.CreateStudent(group.GroupName, semester.Name, false, true, 10);
+        var teacher = EntitiesFactory.CreateTeacher(permissions: TeacherPermissions.SuperUser);
+        var date = DateOnly.FromDateTime(DateTime.Now).AddDays(-8);
+        var historyEntity = EntitiesFactory.CreatePointsStudentHistoryEntity(student.StudentGuid, WorkType.Activist, teacher.TeacherGuid, DateOnlyGenerator.GetWorkingDate(date), 10, semester.Name);
+
+        await context.Semesters.AddAsync(semester);
+        await context.Groups.AddAsync(group);
+        await context.Students.AddAsync(student); 
+        await context.Teachers.AddAsync(teacher);
+        await context.PointsStudentsHistory.AddAsync(historyEntity);
+        await context.SaveChangesAsync();
+        
+        var historyObj = context.PointsStudentsHistory.FirstOrDefault(h => h.Points == historyEntity.Points);
+
+        var payload = new DeletePointsCommandPayload
+        {
+            StudentGuid = historyEntity.StudentGuid,
+            TeacherGuid = historyEntity.TeacherGuid,
+            HistoryId = historyObj!.Id,
+            IgnoreDateIntervalCheck = true,
+        };
+
+        // Act
+        var result = await command.ExecuteAsync(payload);
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+        await using var assertContext = CreateContext();
+        var studentFromDb = await assertContext.Students.Include(s => s.PointsStudentHistory)
+            .Where(s => s.StudentGuid == student.StudentGuid).FirstOrDefaultAsync();
+        Assert.NotNull(studentFromDb);
+        Assert.Equal(expectedAdditionalPoints, studentFromDb.AdditionalPoints);
+        var historyEntityFromDb = studentFromDb.PointsStudentHistory.FirstOrDefault(h => h.Points == historyEntity.Points);
+        Assert.Null(historyEntityFromDb);
+    }
+    
+    [Fact]
+    public async Task DeletePointsAsync_PointsOutdatedException_ShouldThrow()
+    {
+        // Arrange
+        await using var context = CreateContext();
+        await ClearDatabase(context);
+        
+        
+        var command = new DeletePointsCommand(context);
+        var semester = EntitiesFactory.CreateSemester("2022-2023/spring", true);
+        var group = EntitiesFactory.CreateGroup("211-729");
+        var student = EntitiesFactory.CreateStudent(group.GroupName, semester.Name, false, true, 10);
+        var teacher = EntitiesFactory.CreateTeacher(permissions: TeacherPermissions.SuperUser);
+        var date = DateOnly.FromDateTime(DateTime.Now).AddDays(-8);
+        var historyEntity = EntitiesFactory.CreatePointsStudentHistoryEntity(student.StudentGuid, WorkType.Activist, teacher.TeacherGuid, DateOnlyGenerator.GetWorkingDate(date), 10, semester.Name);
+
+        await context.Semesters.AddAsync(semester);
+        await context.Groups.AddAsync(group);
+        await context.Students.AddAsync(student); 
+        await context.Teachers.AddAsync(teacher);
+        await context.PointsStudentsHistory.AddAsync(historyEntity);
+        await context.SaveChangesAsync();
+        
+        var historyObj = context.PointsStudentsHistory.FirstOrDefault(h => h.Points == historyEntity.Points);
+
+        var payload = new DeletePointsCommandPayload
+        {
+            StudentGuid = historyEntity.StudentGuid,
+            TeacherGuid = historyEntity.TeacherGuid,
+            HistoryId = historyObj!.Id,
+            IgnoreDateIntervalCheck = false,
+        };
+
+        // Act
+        var result = await command.ExecuteAsync(payload);
+        
+        // Assert
+        Assert.False(result.IsSuccess);
+        result.Match(_ => true, exception =>
+        {
+            Assert.IsType<PointsOutdatedException>(exception);
+            return true;
+        });
+    }
+    
+    [Fact]
+    public async Task DeletePointsAsync_PointsStudentHistoryNotFoundException_ShouldThrow()
+    {
+        // Arrange
+        await using var context = CreateContext();
+        await ClearDatabase(context);
+
+        var command = new DeletePointsCommand(context);
+        var semester = EntitiesFactory.CreateSemester("2022-2023/spring", true);
+        var group = EntitiesFactory.CreateGroup("211-729");
+        var student = EntitiesFactory.CreateStudent(group.GroupName, semester.Name, false, true, 10);
+        var teacher = EntitiesFactory.CreateTeacher(permissions: TeacherPermissions.SuperUser);
+        var historyEntity = EntitiesFactory.CreatePointsStudentHistoryEntity(student.StudentGuid, WorkType.Activist, teacher.TeacherGuid, DateOnlyGenerator.GetWorkingDate(), 10, semester.Name);
+
+        await context.Semesters.AddAsync(semester);
+        await context.Groups.AddAsync(group);
+        await context.Students.AddAsync(student); 
+        await context.Teachers.AddAsync(teacher);
+        await context.SaveChangesAsync();
+
+        var payload = new DeletePointsCommandPayload
+        {
+            StudentGuid = historyEntity.StudentGuid,
+            TeacherGuid = historyEntity.TeacherGuid,
+            HistoryId = 0,
+            IgnoreDateIntervalCheck = false,
+        };
+
+        // Act
+        var result = await command.ExecuteAsync(payload);
+        
+        // Assert
+        Assert.False(result.IsSuccess);
+        result.Match(_ => true, exception =>
+        {
+            Assert.IsType<PointsStudentHistoryNotFoundException>(exception);
+            return true;
+        });
+    }
+    
+    [Fact]
+    public async Task DeletePointsAsync_StudentNotFoundException_ShouldThrow()
+    {
+        // Arrange
+        await using var context = CreateContext();
+        await ClearDatabase(context);
+
+        var command = new DeletePointsCommand(context);
+        var semester = EntitiesFactory.CreateSemester("2022-2023/spring", true);
+        var group = EntitiesFactory.CreateGroup("211-729");
+        var student = EntitiesFactory.CreateStudent(group.GroupName, semester.Name, false, true, 10);
+        var teacher = EntitiesFactory.CreateTeacher(permissions: TeacherPermissions.SuperUser);
+        var historyEntity = EntitiesFactory.CreatePointsStudentHistoryEntity(student.StudentGuid, WorkType.Activist, teacher.TeacherGuid, DateOnlyGenerator.GetWorkingDate(), 10, semester.Name);
+
+        await context.Semesters.AddAsync(semester);
+        await context.Groups.AddAsync(group);
+        await context.Students.AddAsync(student); 
+        await context.Teachers.AddAsync(teacher);
+        await context.PointsStudentsHistory.AddAsync(historyEntity);
+        await context.SaveChangesAsync();
+        
+        var historyObj = context.PointsStudentsHistory.FirstOrDefault(h => h.Points == historyEntity.Points);
+        var payload = new DeletePointsCommandPayload
+        {
+            StudentGuid = "historyEntity.StudentGuid",
+            TeacherGuid = historyEntity.TeacherGuid,
+            HistoryId = historyObj!.Id,
+            IgnoreDateIntervalCheck = false,
+        };
+
+        // Act
+        var result = await command.ExecuteAsync(payload);
+        
+        // Assert
+        Assert.False(result.IsSuccess);
+        result.Match(_ => true, exception =>
+        {
+            Assert.IsType<StudentNotFoundException>(exception);
+            return true;
+        });
     }
 }
