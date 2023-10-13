3c89385dd16e90b08c145403d5da377c50fc20e3
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-10-14 00:55:46 +0300

    Новые ошибки

---
+++ PhysEdJournal.Core/Exceptions/PointsExceptions/PointsStudentHistoryNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace PhysEdJournal.Core.Exceptions.PointsExceptions;
+
+public sealed class PointsStudentHistoryNotFoundException : Exception
+{
+    public PointsStudentHistoryNotFoundException(int historyId, string studentGuid) 
+        : base($"No history with id = {historyId} in student with guid {studentGuid}") {}
+}
