ea35ea3a832a35816e5d1026b11d55b0e1316358
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-10-17 16:57:07 +0300

    feature/delete-visits-command

--- PhysEdJournal.Core/Constants/VisitConstants.cs
+++ PhysEdJournal.Core/Constants/VisitConstants.cs
@@ -3,4 +3,5 @@
 public static class VisitConstants
 {
     public const int VISIT_LIFE_DAYS = 7;
+    public const int DAYS_TO_DELETE_VISIT = 7;
 }
---
+++ PhysEdJournal.Core/Exceptions/VisitsExceptions/VisitOutdatedException.cs
@@ -0,0 +1,7 @@
+﻿namespace PhysEdJournal.Core.Exceptions.VisitsExceptions;
+
+public class VisitOutdatedException : Exception
+{
+    public VisitOutdatedException(int amountOfDays)
+        : base($"You cannot delete a visit if it existed more than {amountOfDays} days ago") { }
+}
