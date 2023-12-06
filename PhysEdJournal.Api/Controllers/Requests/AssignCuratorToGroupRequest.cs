5a7bb341aef26a6258167347e10ec4ce4ca8d446
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-12-07 00:54:28 +0300

    added AssignCuratorToGroupRequest

---
+++ PhysEdJournal.Api/Controllers/Requests/AssignCuratorToGroupRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace PhysEdJournal.Api.Controllers.Requests;
+
+public sealed class AssignCuratorToGroupRequest
+{
+    public required string GroupName { get; init; }
+
+    public required string TeacherGuid { get; init; }
+
+    public required ClaimsPrincipal ClaimsPrincipal { get; init; }
+}
