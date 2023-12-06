f32fa24de85a232f8e4269e52c82c4aee5978b12
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-12-07 00:54:10 +0300

    added AssignVisitValueRequest.cs

---
+++ PhysEdJournal.Api/Controllers/Requests/AssignVisitValueRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace PhysEdJournal.Api.Controllers.Requests;
+
+public sealed class AssignVisitValueRequest
+{
+    public required string GroupName { get; init; }
+
+    public required double NewVisitValue { get; init; }
+
+    public required ClaimsPrincipal ClaimsPrincipal { get; init; }
+}
