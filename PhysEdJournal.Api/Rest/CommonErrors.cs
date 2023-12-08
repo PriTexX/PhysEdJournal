848cc856fae94b06f0bbbb2c99391d0894994b38
BR7RKR <leomyts@gmail.com>
2023-12-08 18:41:22 +0300

    Добавил новые common ошибки

--- PhysEdJournal.Api/Rest/CommonErrors.cs
+++ PhysEdJournal.Api/Rest/CommonErrors.cs
@@ -1,4 +1,6 @@
+using PhysEdJournal.Core.Exceptions;
 using PhysEdJournal.Core.Exceptions.DateExceptions;
+using PhysEdJournal.Core.Exceptions.PointsExceptions;
 using PhysEdJournal.Core.Exceptions.StudentExceptions;
 using PhysEdJournal.Core.Exceptions.TeacherExceptions;
 
@@ -65,5 +67,89 @@ public static class CommonErrors
                     };
                 }
             },
+            {
+                nameof(DateExpiredException),
+                err =>
+                {
+                    var message = ((DateExpiredException)err).Message;
+                    return new ProblemDetailsResponse
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Type = "bad-request",
+                        Title = "Action was performed after the deadline",
+                        Detail = $"{message}"
+                    };
+                }
+            },
+            {
+                nameof(NegativePointAmount),
+                err =>
+                {
+                    var message = ((NegativePointAmount)err).Message;
+                    return new ProblemDetailsResponse
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Type = "bad-request",
+                        Title = "Attempt to perform action after the deadline",
+                        Detail = $"{message}"
+                    };
+                }
+            },
+            {
+                nameof(NonWorkingDayException),
+                err => 
+                {
+                    var message = ((NonWorkingDayException)err).Message;
+                    return new ProblemDetailsResponse
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Type = "bad-request",
+                        Title = "Attempt to perform action on the non working day",
+                        Detail = $"{message}"
+                    };
+                }
+            },
+            {
+                nameof(ConcurrencyError),
+                err =>
+                {
+                    var message = ((ConcurrencyError)err).Message;
+                    return new ProblemDetailsResponse
+                    {
+                        Status = StatusCodes.Status409Conflict,
+                        Type = "conflict",
+                        Title = "Attempt to perform one action twice",
+                        Detail = $"{message}"
+                    };
+                }
+            },
+            {
+                nameof(TeacherGuidMismatchException),
+                err =>
+                {
+                    var message = ((TeacherGuidMismatchException)err).Message;
+                    return new ProblemDetailsResponse
+                    {
+                        Status = StatusCodes.Status409Conflict,
+                        Type = "conflict",
+                        Title = "This action should be done by another teacher",
+                        Detail = $"{message}"
+                    };
+                }
+            },
+            {
+                nameof(ArchivedPointsDeletionException),
+                err =>
+                {
+                    var message = ((ArchivedPointsDeletionException)err).Message;
+                    return new ProblemDetailsResponse
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Type = "bad-request",
+                        Title = "Attempt to delete archived points",
+                        Detail = $"{message}"
+                    };
+                }
+            }
         };
 }
