4ad159066fb0947d40eff2a2985b97e05eba5200
BR7RKR <leomyts@gmail.com>
2023-12-12 12:53:30 +0300

    Добавил новые константы

--- PhysEdJournal.Core/Constants/PointsConstants.cs
+++ PhysEdJournal.Core/Constants/PointsConstants.cs
@@ -6,7 +6,10 @@ public static class PointsConstants
     public const int DAYS_TO_DELETE_VISITS = 7;
     public const int DAYS_TO_DELETE_POINTS = 30;
     public const int POINTS_LIFE_DAYS = 30;
-    public const int REQUIRED_POINT_AMOUNT = 50;
+    public const int REQUIRED_POINTS_AMOUNT = 50;
+    public const int MaxPointsAmount = 50;
+    public const int MaxPointsForExternalFitness = 10;
+    public const int MaxPointsForScience = 30;
     public const int MAX_POINTS_FOR_ONE_STANDARD = 10;
 
     public static double CalculateTotalPoints(
