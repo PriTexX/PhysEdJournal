a9edb15906ab5d7ed1cdd09dc031e8b06a025049
BR7RKR <leomyts@gmail.com>
2023-12-12 12:13:22 +0300

    Переместил валидатор внутрь реквеста AssignVisitValueRequest

--- PhysEdJournal.Api/Api/Group/Contracts/AssignVisitValueRequest.cs
+++ PhysEdJournal.Api/Api/Group/Contracts/AssignVisitValueRequest.cs
@@ -7,13 +7,13 @@ public sealed class AssignVisitValueRequest
     public required string GroupName { get; init; }
 
     public required double NewVisitValue { get; init; }
-}
 
-public sealed class AssignVisitValueValidator : AbstractValidator<AssignVisitValueRequest>
-{
-    public AssignVisitValueValidator()
+    public sealed class Validator : AbstractValidator<AssignVisitValueRequest>
     {
-        RuleFor(request => request.GroupName).NotEmpty();
-        RuleFor(request => request.NewVisitValue).NotEmpty().GreaterThan(0);
+        public Validator()
+        {
+            RuleFor(request => request.GroupName).NotEmpty();
+            RuleFor(request => request.NewVisitValue).NotEmpty().GreaterThan(0);
+        }
     }
 }
