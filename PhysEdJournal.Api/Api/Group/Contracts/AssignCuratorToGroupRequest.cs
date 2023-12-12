7af04d20d5aa02cb0447ea47701cfe1b9e4ccbf9
BR7RKR <leomyts@gmail.com>
2023-12-12 12:59:44 +0300

    Добавил валидатор в AssignCuratorToGroupRequest

--- PhysEdJournal.Api/Api/Group/Contracts/AssignCuratorToGroupRequest.cs
+++ PhysEdJournal.Api/Api/Group/Contracts/AssignCuratorToGroupRequest.cs
@@ -1,8 +1,19 @@
-﻿namespace PhysEdJournal.Api.Api.Group.Contracts;
+﻿using FluentValidation;
+
+namespace PhysEdJournal.Api.Api.Group.Contracts;
 
 public sealed class AssignCuratorToGroupRequest
 {
     public required string GroupName { get; init; }
 
     public required string TeacherGuid { get; init; }
+
+    public sealed class Validator : AbstractValidator<AssignCuratorToGroupRequest>
+    {
+        public Validator()
+        {
+            RuleFor(request => request.GroupName).NotEmpty();
+            RuleFor(request => request.TeacherGuid).NotEmpty();
+        }
+    }
 }
