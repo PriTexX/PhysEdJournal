c733bb47f95534e8e7dc49fc0af2ed9cfe43579c
LoykoD <90799651+LoykoD@users.noreply.github.com>
2023-12-15 23:35:02 +0300

    upd SetCuratorsFromFile

--- PhysEdJournal.Infrastructure/Commands/AdminCommands/SetCuratorsFromFile.cs
+++ PhysEdJournal.Infrastructure/Commands/AdminCommands/SetCuratorsFromFile.cs
@@ -1,67 +1,3 @@
-<<<<<<< Updated upstream
-﻿using Microsoft.EntityFrameworkCore;
-using PhysEdJournal.Core.Exceptions.GroupExceptions;
-using PhysEdJournal.Core.Exceptions.TeacherExceptions;
-using PhysEdJournal.Infrastructure.Commands.ValidationAndCommandAbstractions;
-using PhysEdJournal.Infrastructure.Database;
-using PResult;
-
-namespace PhysEdJournal.Infrastructure.Commands.AdminCommands;
-
-public sealed class SetCuratorsFromFileCommandPayload
-{
-    public required byte[] PayloadFile { get; init; } // csv file
-    public required string TeacherGuid { get; init; } // Список препод
-}
-
-public sealed class SetCuratorsFromFileCommand : ICommand<SetCuratorsFromFileCommandPayload, Unit>
-{
-    private readonly ApplicationContext _applicationContext;
-
-    public SetCuratorsFromFileCommand(ApplicationContext applicationContext)
-    {
-        _applicationContext = applicationContext;
-    }
-
-    public async Task<Result<Unit>> ExecuteAsync(SetCuratorsFromFileCommandPayload commandPayload)
-    {
-        var rowsAffected = await _applicationContext.Teachers.ToListAsync(); // Получаю список всех преподов
-        var file = commandPayload.PayloadFile; // Получаю csv и прохожусь по нему
-        for (var i = 0; i < file.Length; i++)
-        {
-            string[] rowData = file[i].ToString().Split(';'); // Разделяю строку по ;
-            string numberGroup = rowData[0]; // Получаю номер группы, имя, стоимость посещения
-            string fullName = rowData[1];
-            double visitCost = double.Parse(rowData[2]); // Если первая строка с описанием столбцов, то скип
-
-            if (numberGroup == "Номер группы")
-            {
-                continue;
-            }
-
-            if (rowsAffected.Exists(x => x.FullName == fullName))
-            {
-                var teacher = await _applicationContext.Teachers.FindAsync(fullName); // Достаю из БД препода и проверяю что он существует
-                if (teacher is null)
-                {
-                    return new TeacherNotFoundException(commandPayload.TeacherGuid);
-                }
-
-                var group = await _applicationContext.Groups.FindAsync(numberGroup); // Проверяю есть ли группа
-
-                if (group is null)
-                {
-                    return new GroupNotFoundException(numberGroup);
-                }
-
-                group.Curator = teacher; // Присваиваю кураторку
-                group.CuratorGuid = teacher.TeacherGuid;
-            }
-        }
-
-        return Unit.Default;
-    }
-=======
 ﻿using System.Globalization;
 using CsvHelper;
 using CsvHelper.Configuration;
@@ -141,5 +77,4 @@ public sealed class SetCuratorsFromFileCommand : ICommand<SetCuratorsFromFileCom
         await _applicationContext.SaveChangesAsync();
         return Unit.Default;
     }
->>>>>>> Stashed changes
 }
