15642c08b7fe2e12c807895335e6b3d5bedde73c
BR7RKR <leomyts@gmail.com>
2023-12-21 14:34:54 +0300

    added AddPointsForStandardToStudentRequest

---
+++ PhysEdJournal.Api/Api/AddPoints/Contracts/AddPointsForStandardToStudentRequest.cs
@@ -0,0 +1,30 @@
+using FluentValidation;
+using PhysEdJournal.Core.Entities.Types;
+using static PhysEdJournal.Core.Constants.PointsConstants;
+
+namespace PhysEdJournal.Api.Api.AddPoints.Contracts;
+
+public class AddPointsForStandardToStudentRequest
+{
+    public required string StudentGuid { get; init; }
+    public required int PointsAmount { get; init; }
+    public required DateOnly Date { get; init; }
+    public required StandardType StandardType { get; init; }
+    public required bool IsOverride { get; init; }
+    public string? Comment { get; init; } = null;
+
+    public sealed class Validator : AbstractValidator<AddPointsForStandardToStudentRequest>
+    {
+        public Validator()
+        {
+            RuleFor(request => request.StudentGuid).NotEmpty();
+            RuleFor(request => request.PointsAmount)
+                .NotEmpty()
+                .GreaterThan(0)
+                .LessThan(MAX_POINTS_FOR_ONE_STANDARD);
+            RuleFor(request => request.Date).NotEmpty();
+            RuleFor(request => request.StandardType).NotEmpty();
+            RuleFor(request => request.IsOverride).NotEmpty();
+        }
+    }
+}
