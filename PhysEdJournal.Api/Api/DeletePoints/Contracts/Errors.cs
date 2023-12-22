897abc0f6e611c6f9cf7e8e62678a61b08f03184
BR7RKR <leomyts@gmail.com>
2023-12-22 14:09:10 +0300

    Добавил все ошибки для DeletePointsController

--- PhysEdJournal.Api/Api/DeletePoints/Contracts/Errors.cs
+++ PhysEdJournal.Api/Api/DeletePoints/Contracts/Errors.cs
@@ -1,3 +1,5 @@
+using PhysEdJournal.Core.Exceptions.PointsExceptions;
+using PhysEdJournal.Core.Exceptions.StandardExceptions;
 using PhysEdJournal.Core.Exceptions.VisitsExceptions;
 
 namespace PhysEdJournal.Api.Api.DeletePoints.Contracts;
@@ -38,6 +40,28 @@ public static class DeletePointsErrors
                         Type = "visit-outdated",
                         Title = "Attempt to delete outdated visit",
                         Detail = "You cannot delete visits that were set too long ago",
+                    }
+            },
+            {
+                nameof(PointsStudentHistoryNotFoundException),
+                _ =>
+                    new ProblemDetailsResponse
+                    {
+                        Status = StatusCodes.Status404NotFound,
+                        Type = "points-history-not-found",
+                        Title = "Points student history not found",
+                        Detail = "Points student history record was not found in the database",
+                    }
+            },
+            {
+                nameof(StandardsStudentHistoryNotFoundException),
+                _ =>
+                    new ProblemDetailsResponse
+                    {
+                        Status = StatusCodes.Status404NotFound,
+                        Type = "standards-history-not-found",
+                        Title = "Standards student history not found",
+                        Detail = "Standards student history record was not found in the database",
                     }
             },
         };
