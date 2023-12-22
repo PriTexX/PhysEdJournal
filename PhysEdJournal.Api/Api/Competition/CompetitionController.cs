fd205ecba26dcc9cad55b2fe439dce0be7d29184
BR7RKR <leomyts@gmail.com>
2023-12-22 14:31:08 +0300

    added CompetitionController

--- PhysEdJournal.Api/Api/Competition/CompetitionController.cs
+++ PhysEdJournal.Api/Api/Competition/CompetitionController.cs
@@ -1,3 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using PhysEdJournal.Api.Api.Competition.Contracts;
+using PhysEdJournal.Api.Controllers;
+using PhysEdJournal.Infrastructure.Commands.AdminCommands;
+using static PhysEdJournal.Core.Constants.PermissionConstants;
+
 namespace PhysEdJournal.Api.Api.Competition;
 
-public static class CompetitionController { }
+public static class CompetitionController
+{
+    public static void MapCompetitionEndpoints(IEndpointRouteBuilder router)
+    {
+        ErrorHandler.AddErrors(CompetitionErrors.Errors);
+
+        router.MapPost("/CreateCompetition", CreateCompetition);
+        router.MapDelete("/DeleteCompetition", DeleteCompetition);
+    }
+
+    public static async Task<IResult> CreateCompetition(
+        string competitionName,
+        [FromServices] CreateCompetitionCommand createCompetitionCommand,
+        [FromServices] PermissionValidator permissionValidator,
+        HttpContext ctx
+    )
+    {
+        var callerGuid = ctx.User.Claims.First(c => c.Type == "IndividualGuid").Value;
+
+        await permissionValidator.ValidateTeacherPermissionsAndThrow(
+            callerGuid,
+            FOR_ONLY_ADMIN_USE_PERMISSIONS
+        );
+
+        var result = await createCompetitionCommand.ExecuteAsync(competitionName);
+
+        return result.Match(_ => Results.Ok(), ErrorHandler.HandleErrorResult);
+    }
+
+    public static async Task<IResult> DeleteCompetition(
+        string competitionName,
+        [FromServices] DeleteCompetitionCommand deleteCompetitionCommand,
+        [FromServices] PermissionValidator permissionValidator,
+        HttpContext ctx
+    )
+    {
+        var callerGuid = ctx.User.Claims.First(c => c.Type == "IndividualGuid").Value;
+
+        await permissionValidator.ValidateTeacherPermissionsAndThrow(
+            callerGuid,
+            FOR_ONLY_ADMIN_USE_PERMISSIONS
+        );
+
+        var result = await deleteCompetitionCommand.ExecuteAsync(competitionName);
+
+        return result.Match(_ => Results.Ok(), ErrorHandler.HandleErrorResult);
+    }
+}
