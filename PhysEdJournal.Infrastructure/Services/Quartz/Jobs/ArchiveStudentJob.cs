32a2865182265fec4f632ba6b9ff104c03c7f5b9
pritexx <vladon_2004@mail.ru>
2024-01-11 11:32:21 +0300

    fixed error

--- PhysEdJournal.Infrastructure/Services/Quartz/Jobs/ArchiveStudentJob.cs
+++ PhysEdJournal.Infrastructure/Services/Quartz/Jobs/ArchiveStudentJob.cs
@@ -38,6 +38,10 @@ public sealed class ArchiveStudentJob : IJob
                 .Where(s => s.HasDebtFromPreviousSemester)
                 .ToListAsync();
 
+            var activeSemester = await _applicationContext.Semesters
+                .Where(s => s.IsCurrent)
+                .FirstAsync();
+
             totalStudents = students.Count;
             foreach (var stud in students)
             {
@@ -49,6 +53,7 @@ public sealed class ArchiveStudentJob : IJob
                 var archivePayload = new ArchiveStudentCommandPayload
                 {
                     StudentGuid = stud.StudentGuid,
+                    SemesterName = activeSemester.Name,
                 };
 
                 try
