9713b3723dc39f52797300450f019e3de1ba424c
BR7RKR <leomyts@gmail.com>
2024-03-11 15:58:03 +0300

    Написал класс для параметров пагинации

--- PhysEdJournal.Api/Rest/Common/Extensions/RouteHandlerBuilderExtensions.cs
+++ PhysEdJournal.Api/Rest/Common/Extensions/RouteHandlerBuilderExtensions.cs
@@ -4,7 +4,8 @@ namespace PhysEdJournal.Api.Rest.Common.Extensions;
 
 public static class RouteHandlerBuilderExtensions
 {
-    public static RouteHandlerBuilder AddValidation<TReq, TVal>(this RouteHandlerBuilder builder) where TVal : IValidator<TReq> 
+    public static RouteHandlerBuilder AddValidation<TReq, TVal>(this RouteHandlerBuilder builder)
+        where TVal : IValidator<TReq>
     {
         return builder.AddEndpointFilter<ValidationFilter<TReq, TVal>>();
     }
---
+++ PhysEdJournal.Api/Rest/Common/Pagination/PaginationParameters.cs
@@ -0,0 +1,7 @@
+namespace PhysEdJournal.Api.Rest.Common.Pagination;
+
+public class PaginationParameters
+{
+    public required int PageNumber { get; init; }
+    public required int PageSize { get; init; }
+}
