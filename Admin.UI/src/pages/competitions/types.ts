e556d265d05f6d7bbacb4ee589aa6f403db42c0f
pritexx <vladon_2004@mail.ru>
2024-05-28 11:32:56 +0300

    WIP

---
+++ Admin.UI/src/pages/competitions/types.ts
@@ -0,0 +1,3 @@
+export type Competition = {
+  competitionName: string;
+};
---
+++ Admin.UI/src/pages/standards/types.ts
@@ -0,0 +1,9 @@
+export type Standard = {
+  id: number;
+  date: Date;
+  points: number;
+  teacherGuid: string;
+  standardType: '';
+  comment: string | null;
+  studentGuid: string;
+};
